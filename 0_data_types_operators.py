7998b4e7799bdd9783750b66fcd5e4f35b4d3dab
natalibogarin <natali.bogarin@gmail.com>
2020-08-12 19:46:11 -0300

    Agregada documentacion de Tipos de datos y Operadores

---
+++ 0_data_types_operators.py
@@ -0,0 +1,11 @@
+#Concatenación
+concatenacion = "Hola " + "mundo"
+print("Resultado concatenación:", concatenacion)
+
+#Multiplicación
+multiplicacion = 'Hola' * 3
+print("Resultado multiplicación:", multiplicacion)
+
+#Mezcla
+mezcla = 'Hola' * 3 + ' mundo'
+print("Resultado mezcla:", mezcla)
